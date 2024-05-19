906e31098c07aba7a2105031b2c09451235d716b
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-05-19 06:40:53 -0600

    Update brain.sh

--- brain.sh
+++ brain.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Set permissions for the script files
-chmod 755 1.sh 2.sh 3.sh 4.sh 5.sh
+chmod 777 1.sh 2.sh 3.sh 4.sh 5.sh
 
 # Execute each script using sh
-sh 1-*
-sh 2-*
-sh 3-*
-sh 4-*
-sh 5-*
+./1.sh
+sleep 60
+./2.sh
+sleep60
+./3.sh
+sleep 60
+./4.sh
+sleep 60
+./5.sh
